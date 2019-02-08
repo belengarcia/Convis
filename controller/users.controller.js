df961d56ff175570239d20e764abbe2bc6c6cc75
Belen <>
2019-02-08 15:12:01 +0100

    users controller

---
+++ controller/users.controller.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
+const createError = require('http-errors');
+
+module.exports.create = (req, res, next) => {
+    User.findOne({mail: req.body.mail})
+        .then(user => {
+            if(user) {
+                throw createError(409, `User with email: ${req.body.mail} already exists`)
+            } else {
+                user = new User(req.body);
+                user.save()
+                    .then(user => res.status(201).json(user))
+                    .catch(error => next(error))
+            }
+        })
+        .catch(error => next(error));
+}
+
+module.exports.list = (req, res, next) => {
+    User.find()
+        .then(users => {
+            if(users) {
+                res.json(users)
+            } else {
+                throw createError(404, 'Users not found')
+            }
+        })
+        .catch(error => next(error))
+}
+
+module.exports.get = (req, res, next) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                throw createError(404, 'User not found')
+            } else {
+                res.json(user)
+            }
+        })
+}
+
+module.exports.delete = (req, res, enxt) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(user => {
+            if(!user){
+                throw createError(404, 'User not found');
+            } else {
+                res.status(204).json()
+            }
+        })
+        .catch(error => next(error));
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controller/users.controller')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', userController.create);
+router.get('/', userController.list);
+router.get('/:id', userController.get);
+router.delete('/:id', userController.delete);
 
 module.exports = router;
